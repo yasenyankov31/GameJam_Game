20c409864ebdb611f0ebd9e5ff5fc24239ef1e91
yasenyankov31 <yasenyankov21@gmail.com>
2023-02-04 20:02:18 +0200

    changes

--- Assets/Scripts/Capablities/GrabAndThrow.cs
+++ Assets/Scripts/Capablities/GrabAndThrow.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GrabAndThrow : MonoBehaviour
 {
-    [SerializeField]
-    private Transform grabPoint;
 
     [SerializeField]
     private Transform rayPoint;
@@ -13,8 +11,8 @@ public class GrabAndThrow : MonoBehaviour
     [SerializeField]
     private float rayDistance;
 
-
-    public GameObject grabbedObject;
+        
+    public GameObject cube;
     public float throwForce = 10f;
     // Start is called before the first frame update
     void Start()
@@ -25,22 +23,13 @@ public class GrabAndThrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
+        
 
-        if (Input.GetKeyDown(KeyCode.Mouse1) && hitInfo.collider.gameObject.tag == "Amulet" && grabbedObject == null)
-        {
-            grabbedObject = hitInfo.collider.gameObject;
-            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            grabbedObject.transform.position = grabPoint.position;
-            grabbedObject.transform.SetParent(transform);
-        }
-        else if (Input.GetKeyDown(KeyCode.Mouse1) && grabbedObject != null)
-        {
-            grabbedObject.GetComponent<Rigidbody2D>().isKinematic = false;
-            grabbedObject.GetComponent<Rigidbody2D>().AddForce(grabPoint.right*transform.localScale.x * throwForce, ForceMode2D.Impulse);
-            grabbedObject.transform.SetParent(null);
-            grabbedObject = null;
-        }
+    }
+
+    public void Shoot()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(rayPoint.position, transform.right, rayDistance);
     }
 }
 
