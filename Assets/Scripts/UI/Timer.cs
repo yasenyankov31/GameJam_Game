5e2272aad5889a162f1b4186c4c7d5dc19933677
kirrcho <kirrcho6@gmail.com>
2023-02-05 11:43:59 +0200

    added timer example

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 0;
+    public float Minutes;
+    public float Seconds;
+    public Text timeText;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        DisplayTime(time);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        Minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        Seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", Minutes, Seconds);
+    }
+}
