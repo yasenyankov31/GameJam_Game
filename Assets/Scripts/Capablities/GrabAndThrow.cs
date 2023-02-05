5c0e75f8ac75326232ae7243a54d5d6d10a2df85
yasenyankov31 <yasenyankov21@gmail.com>
2023-02-05 09:17:22 +0200

    changed gameplay and decors

--- Assets/Scripts/Capablities/GrabAndThrow.cs
+++ Assets/Scripts/Capablities/GrabAndThrow.cs
@@ -4,53 +4,24 @@ using UnityEngine;
 
 public class GrabAndThrow : MonoBehaviour
 {
+
+    public Transform firePoint;
     public GameObject amulet;
-    public float launchForce;
-    public Transform shotPoint;
-
-    public GameObject point;
-    GameObject[] points;
-    public int numberOfPoints;
-    public float spaceBetweenPoints;
-    public Vector2 direction;
-
-    private void Start()
-    {
-        points = new GameObject[numberOfPoints];
-        for (int i = 0; i < numberOfPoints; i++)
-        {
-            points[i] = Instantiate(point, shotPoint.position, Quaternion.identity);
-        }
-    }
-
+    public float AmuletSpeed = 10f;
     private void Update()
     {
-        Vector2 bowPosition = transform.position;
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        direction = mousePosition - bowPosition;
-        transform.right = direction;
-
-
-        if (Input.GetMouseButtonDown(0))
+        Shoot();
+    }
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Shoot();
-        }
-        for (int i = 0; i < numberOfPoints; i++)
-        {
-            points[i].transform.position = PointPosition(i * spaceBetweenPoints);
+            float x = GetComponentInParent<PlayerLocomotion>().transform.localScale.x;
+            GameObject amuletIns = Instantiate(amulet, firePoint.position, firePoint.rotation);
+            amuletIns.GetComponent<Rigidbody2D>().AddForce(x * transform.right * AmuletSpeed);
         }
     }
-    public void Shoot()
-    {
-        GameObject newAmulet = Instantiate(amulet, shotPoint.position, shotPoint.rotation);
-        newAmulet.GetComponent<Rigidbody2D>().velocity = transform.right * launchForce;
 
-    }
-    private Vector2 PointPosition(float t)
-    {
-        Vector2 position = (Vector2)shotPoint.position + (direction.normalized *launchForce* t ) + 0.5f * Physics2D.gravity * (t * t);
-        return position;
-    }
 
 }
 
